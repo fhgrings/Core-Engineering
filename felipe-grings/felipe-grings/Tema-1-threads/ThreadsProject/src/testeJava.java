ffe13dfaee5c51c2d8c57aa04841f8ba32c4994b
Felipe Grings <felipe.grings@ilegra.com>
2018-08-17 17:04:16 -0300

    Subindo tema thread

---
+++ felipe-grings/felipe-grings/Tema-1-threads/ThreadsProject/src/testeJava.java
@@ -0,0 +1,12 @@
+public class testeJava {
+
+    public static void main (String[] args) {
+
+        minhaThread threadPar = new minhaThread("par", 500);
+
+        minhaThread threadImpar = new minhaThread("impar", 100);
+
+
+    }
+
+}
