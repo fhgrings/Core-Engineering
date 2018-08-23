829f3f1e8b989f0abc4011d738ff3dd9647bd82f
ViniFKroth <42379563+ViniFKroth@users.noreply.github.com>
2018-08-23 08:06:25 -0300

    Tema 2 threads (#13)
    * Add git ignore file
    
    * Create Lampada project
    
    * Create Thread project
    
    * Revert "Create Thread project"
    
    This reverts commit 8a2e66ad98bf718454c86806b8d78593b1532841.
    
    * Create request threads exercise
    
    * Revert "Create Lampada project"
    
    This reverts commit 114f12bc86ac78ab43eb93bc781383c965efd472.
    
    * Revert "Create Lampada project"
    
    This reverts commit 114f12bc86ac78ab43eb93bc781383c965efd472.
    
    * Resolving problems
    
    * Revert "Resolving problems"
    
    This reverts commit e5ca6ee49cb09547f291a504b7a1a6d86f72cbec.
    
    * Add gitignore in proper location
    
    * Change package name and ajust identation
    
    * Deleting misplaced .gitignore
    
    * Alter .gitignore
    
    * Delete org.eclipse.jdt.core.prefs
    
    Deleting undue files

---
+++ vinicius-kroth/ThreadExample/src/com/github/vinifkroth/coreeng/tema2/threads/ThreadSimulator.java
@@ -0,0 +1,48 @@
+package com.github.vinifkroth.coreeng.tema2.threads;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadSimulator {
+
+	public static void main(String[] args) {
+		Runnable taskEvenNumbers = () -> {
+
+			for (int i = 0; i < 50; i++) {
+				try {
+					TimeUnit.MILLISECONDS.sleep(500);
+					int number = ThreadLocalRandom.current().nextInt(50, 500);
+					if (number % 2 != 0)
+						number++;
+					System.out.println("Even numbers thread : " + number);
+				} catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+				}
+
+			}
+		};
+		
+		Runnable taskOddNumbers = () -> {
+
+			for (int i = 0; i < 50; i++) {
+				try {
+					TimeUnit.MILLISECONDS.sleep(100);
+					int number = ThreadLocalRandom.current().nextInt(50, 500);
+					if (number % 2 == 0)
+						number++;
+					System.out.println("Odd numbers thread : " + number);
+				} catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+				}
+
+			}
+		};	
+
+		Thread threadEvenNumbers = new Thread(taskEvenNumbers);
+		threadEvenNumbers.start();
+
+		Thread threadOddNumbers= new Thread(taskOddNumbers);
+		threadOddNumbers.start();
+
+	}
+
+}
