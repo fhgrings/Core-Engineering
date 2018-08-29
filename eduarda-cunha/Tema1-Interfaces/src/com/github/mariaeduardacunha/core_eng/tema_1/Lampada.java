f2589fbf0a19225ea79c5fe3d32fe062c61f95d6
mariaeduardacunha <42151808+mariaeduardacunha@users.noreply.github.com>
2018-08-29 08:50:08 -0300

    Pullreq tema1 (#53)
    * commit felipe_grings
    
    * inserção do arquivo README.md
    
    * Tema 4 (entrega)
    
    * Create new folder and add readme file
    
    * Teste novo remote
    
    * Adicionando Tema 1 - Interfaces e gitignore

---
+++ eduarda-cunha/Tema1-Interfaces/src/com/github/mariaeduardacunha/core_eng/tema_1/Lampada.java
@@ -0,0 +1,6 @@
+package com.github.mariaeduardacunha.core_eng.tema_1;
+
+public interface Lampada {
+    void on();
+    void off();
+}
---
+++ eduarda-cunha/Tema1-Interfaces/src/com/github/mariaeduardacunha/core_eng/tema_1/Main.java
@@ -0,0 +1,27 @@
+package com.github.mariaeduardacunha.core_eng.tema_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Led led = new Led();
+        Fluorescente fluorescente = new Fluorescente();
+        Interruptor interruptorLed = new Interruptor(led);
+        Interruptor interruptorFluor = new Interruptor(fluorescente);
+
+        if(interruptorFluor.switchLampada()){
+            System.out.println("Fluorescente ligou");
+        }
+        if(!interruptorFluor.switchLampada()){
+            System.out.println("Fluorescente desligou");
+        }
+
+        if(interruptorLed.switchLampada()){
+            System.out.println("LED ligou");
+        }
+        if(!interruptorLed.switchLampada()){
+            System.out.println("LED desligou");
+        }
+
+    }
+}
